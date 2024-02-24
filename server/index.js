afb1821f009dee9a80b8837f10ecf518b099fe39
murshidameen <murshidmonzpv@gmail.com>
2024-02-24 13:08:50 +0530

    express server setup,mongodb connected

--- server/index.js
+++ server/index.js
@@ -1,1 +1,15 @@
-console.log("hiii");
+import express from "express";
+import dotenv from "dotenv";
+import cors from "cors";
+import "./src/db/db.js";
+const app = express();
+dotenv.config();
+const port = process.env.PORT || 3001;
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(cors());
+
+
+app.listen(port, () => {
+  console.log(`Server Listening on port http://localhost:${port}`);
+});  
