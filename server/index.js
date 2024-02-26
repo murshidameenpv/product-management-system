cf28ca0399251602bc909c3c40cebb39eee427e5
murshidameen <murshidmonzpv@gmail.com>
2024-02-26 10:29:39 +0530

    bug fix for deployment

--- server/index.js
+++ server/index.js
@@ -3,9 +3,9 @@ import dotenv from "dotenv";
 import cors from "cors";
 import "./src/db/db.js";
 import cookieParser from "cookie-parser";
-import userRoute from './src/routes/userRoutes.js'
-import authRoute from './src/routes/authRoute.js'
-import productRoute from './src/routes/productRoute.js'
+import userRoute from "./src/routes/userRoutes.js";
+import authRoute from "./src/routes/authRoute.js";
+import productRoute from "./src/routes/productRoute.js";
 const app = express();
 dotenv.config();
 const port = process.env.PORT || 3001;
@@ -18,12 +18,16 @@ app.use(
   })
 );
 app.use(cookieParser());
+app.use(express.static(path.join(__dirname, "../client/dist")));
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "client", "dist", "index.html"));
+});
 //user
 app.use("/api", userRoute);
 app.use("/jwt", authRoute);
 //product
-app.use('/product',productRoute)
+app.use("/product", productRoute);
 
 app.listen(port, () => {
   console.log(`Server Listening on port http://localhost:${port}`);
-});  
+});
